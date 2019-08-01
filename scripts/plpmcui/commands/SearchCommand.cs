60d9485125fdac23bb87db3b78ee90fc4e30549b
Akihisa Yagi <capra314cabra@gmail.com>
2019-08-01 21:56:49 +0900

    Implement a command "search --local"

--- scripts/plpmcui/commands/SearchCommand.cs
+++ scripts/plpmcui/commands/SearchCommand.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 
 namespace PrivateLocatedPackageManager.CUI
 {
@@ -6,19 +8,34 @@ namespace PrivateLocatedPackageManager.CUI
     {
         public string Name => "search";
 
-        public string Usage => "plpm search";
+        public string Usage => "plpm search [--local(optional)]";
 
-        public string Description => "Show the list of all packages created.";
+        public string Description => "Show the list of all packages created.\n" +
+            "When it has \"--local\", that will be replaced with the list of packages which were installed in the working directory.";
 
         public void Execute(string[] args)
         {
-            var finder = new PackageFinder();
-            var list = finder.FindAll();
-
-            Console.WriteLine($"{list.Count} packages found.");
-            foreach(var name in list)
+            if(args.Contains("--local"))
             {
-                Console.WriteLine($"- {name}");
+                var current_dir = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar;
+                var installed = InstalledPackages.Deserialize(current_dir);
+
+                Console.WriteLine($"{installed.Packages.Count} packages found.");
+                foreach(var package in installed.Packages)
+                {
+                    Console.WriteLine($"- {package.Key}");
+                }
+            }
+            else
+            {
+                var finder = new PackageFinder();
+                var list = finder.FindAll();
+
+                Console.WriteLine($"{list.Count} packages found.");
+                foreach(var name in list)
+                {
+                    Console.WriteLine($"- {name}");
+                }
             }
         }
     }
